66e47810ed9dd16ce52e2e1137f47cf5be5ea31e
Adrian Mihaila <111350786+Adrian-Mihaila@users.noreply.github.com>
2023-03-17 08:44:50 +0200

    Update Problems.py

--- Problems.py
+++ Problems.py
@@ -1,6 +1,6 @@
 # Scramblies. Return true if s2 is in s1 or false if not.
 
-# O(n*m)
+# # Rezolvare 1 O(n*m)
 def scramble(s1, s2):
     s1 =list(s1)
     s2 =list(s2)
@@ -14,7 +14,7 @@ def scramble(s1, s2):
 
 print(scramble('cedewaraaossoqqyt', 'codewars'))
 
-# O(n)
+# # Rezolvare 2 O(n)
 def scramble(s1, s2):
     freq = {}
     
@@ -32,3 +32,32 @@ def scramble(s1, s2):
     return True
     
 print(scramble('cedewaraaossoqqyt', 'codewars'))
+
+# Rezolvare 3
+def scramble(s1,s2):
+    for c in set(s2):
+        if s1.count(c) < s2.count(c):
+            return False
+    return True
+
+# Rezolvare 4
+from collections import Counter
+def scramble(s1,s2):
+    # Counter basically creates a dictionary of counts and letters
+    # Using set subtraction, we know that if anything is left over,
+    # something exists in s2 that doesn't exist in s1
+    return len(Counter(s2)- Counter(s1)) == 0
+
+# Rezolvare 5
+def scramble(s1, s2):
+    return not any(s1.count(char) < s2.count(char) for char in set(s2))
+
+# Rezolvare 6
+def scramble(s1,s2):
+    return all( s1.count(x) >= s2.count(x) for x in set(s2))
+
+# Rezolvare 7
+from collections import Counter
+def scramble(s1, s2):
+    # Python 3.10 rich comparison operators for Counter ftw!
+    return Counter(s2) <= Counter(s1)
